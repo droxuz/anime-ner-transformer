77f22c60a21d6d9487ce5b66eeac72d9bed471c0
droxuz <156976891+droxuz@users.noreply.github.com>
2026-07-16 18:36:22 -0400

    MLM dataset complete

--- mlm_dataset.py
+++ mlm_dataset.py
@@ -2,4 +2,61 @@ import torch
 from torch.utils.data import Dataset
 
 class MLMDataset(Dataset):
-    def __init__()
+    def __init__(self, data, tokenizer, max_len, mask_probability):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.mask_probability = mask_probability
+        
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        cls_id = self.tokenizer.token_to_id("[CLS]")
+        sep_id = self.tokenizer.token_to_id("[SEP]")
+        pad_id = self.tokenizer.token_to_id("[PAD]")
+        mask_id = self.tokenizer.token_to_id("[MASK]")
+        unk_id = self.tokenizer.token_to_id("[UNK]")
+
+        # Adding of the [SEP] [CLS] [UNK] Tags to the synopses
+        # Max len of synopsis is 325 will use 384 max_len 
+        x_synopsis = self.data[idx]
+        synopsis_token_object = self.tokenizer.encode(x_synopsis)
+        synopsis_tokens = synopsis_token_object.tokens
+        synopsis_ids = synopsis_token_object.ids
+        synopsis_attention = synopsis_token_object.attention_mask
+
+        # Lossy will lose information on this (Maybe remove the entire entry? if so)
+        if len(synopsis_ids) > (self.max_len - 2):
+            synopsis_tokens = synopsis_tokens[:self.max_len-2]
+            synopsis_ids = synopsis_ids[:self.max_len-2]
+            synopsis_attention = synopsis_attention[:self.max_len-2]
+        synopsis_ids = [cls_id] + synopsis_ids + [sep_id]
+        synopsis_tokens = ["[CLS]"] + synopsis_tokens + ["[SEP]"]
+        synopsis_token_length = len(synopsis_ids)
+
+        # Add the padding to the entire max_len of the synopsis tokens
+        padding_len = self.max_len - synopsis_token_length 
+        padding_tokens = ["[PAD]"] * padding_len
+        padding_ids = [pad_id] * padding_len
+        synopsis_tokens = synopsis_tokens + padding_tokens
+        synopsis_ids = synopsis_ids + padding_ids
+
+        # Attention Masking
+        attention_mask = ["-100"] * padding_len
+        synopsis_attention = synopsis_attention + attention_mask
+
+        return {
+            "synopsis_tokens": torch.tensor(synopsis_tokens, dtype= torch.StringType), 
+            "synopsis_ids": torch.tensor(synopsis_ids, dtype= torch.long),
+            "synopsis_attention": torch.tensor(synopsis_attention, dtype= torch.long)
+            }
+
+
+
+
+
+
+
+
--- pretrained_bpe.py
+++ pretrained_bpe.py
@@ -17,7 +17,7 @@ pretrained_bpe_path = "data/anime_training_data/synopsis_pretrained_bpe_tokenize
 #training the BPE using the data
 def train_bpe_tokenizer(data, save_path, vocab_size = 50000):
 
-    tokenizer = Tokenizer(BPE(unk_token="<UNK>"))
+    tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
 
     # Normalize data, Pre-tokenize Keep the Capitalization for similar to BERT case
     # tokenizer.normalizer = Sequence([NFKC(),Lowercase()])
@@ -25,7 +25,7 @@ def train_bpe_tokenizer(data, save_path, vocab_size = 50000):
     tokenizer.decoder = ByteLevelDecoder()
 
     #Define training
-    trainer = BpeTrainer( min_frequency=2, special_tokens=["<PAD>", "<UNK>"], initial_alphabet=ByteLevel.alphabet())
+    trainer = BpeTrainer( min_frequency=2, special_tokens=["[PAD]", "[UNK]", "[CLS]", "[SEP]"], initial_alphabet=ByteLevel.alphabet())
     tokenizer.train_from_iterator(data, trainer= trainer)
     tokenizer.save(save_path)
 
@@ -45,14 +45,14 @@ def get_bpe_tokenizer(data, save_path, vocab_size=50000, force_retrain=False):
 # synopsis_data = load_jsonl(mal_synopsis_path)
 # BPE_tokens = train_bpe_tokenizer(synopsis_data, pretrained_bpe_path)
 
-tokenizer = load_bpe_tokenizer(pretrained_bpe_path)
+# tokenizer = load_bpe_tokenizer(pretrained_bpe_path)
 # print("Actual vocab size:", tokenizer.get_vocab_size())
 
-test_titles = ["Attack on Titan", "Steins;Gate", "Re:Zero", "Fullmetal Alchemist"]
-for title in test_titles:
-    count = 0
-    encoded = tokenizer.encode(title)
-    print(f"{title:30} → {encoded.tokens}")
-    for token in encoded.tokens:
-        count += 1 
-    print(f"Tokens #: {count}")
+# test_titles = ["Attack on Titan", "Steins;Gate", "Re:Zero", "Fullmetal Alchemist"]
+# for title in test_titles:
+#     count = 0
+#     encoded = tokenizer.encode(title)
+#     print(f"{title:30} → {encoded.tokens}")
+#     for token in encoded.tokens:
+#         count += 1 
+#     print(f"Tokens #: {count}")
